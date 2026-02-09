c78dfc3c76a58e511d20c22ecdb32e63c1e26078
Amir Hamza <amirhamza27940@gmail.com>
2026-02-09 08:11:12 +0600

    contact page done

--- client/src/app/UI/Button.tsx
+++ client/src/app/UI/Button.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 
 interface Type {
     path:string,
-    label:string,
+    label:string
 }
 
-const Button:React.FC<Type> = ({path,label}) => {
+const Button:React.FC<Type> = ({path="/" ,label,}) => {
   return (
     <Link href={path}
-    className='text-white text-lg max-w-fit uppercase font-semibold px-9 py-3.5 rounded-full bg-primary hover:bg-secondary transition-colors duration-300 ' >{label}</Link>
+    className='text-white text-xl max-w-fit uppercase font-semibold px-9 py-3.5 rounded-full bg-primary hover:bg-secondary transition-colors duration-300 ' >
+      <button type="submit" className='cursor-pointer' >{label}</button>
+    </Link>
   )
 }
 
--- client/src/app/contact-us/page.tsx
+++ client/src/app/contact-us/page.tsx
@@ -1,3 +1,4 @@
+import Button from 'app/UI/Button'
 import PageBanner from 'app/UI/PageBanner'
 import Title from 'app/UI/Title'
 import { contact_details } from 'config/page'
@@ -11,7 +12,7 @@ const Contact = () => {
   return (
     <div className="grid w-full h-full">
         <PageBanner title='Contact Us' path='/contact-us'/>
-        <div className="container flex flex-col w-full h-full mx-auto my-12 ">
+        <div className="container flex flex-col  w-full h-full mx-auto my-12 ">
             <div className="flex flex-wrap items-center justify-center w-full gap-5 my-12 h-full">
                 {contact_details.map((item, index)=>(
                     <div key={index} className="flex w-full max-w-100 border-[0.5px] border-neutral-300 rounded-md min-h-136 items-start  shadow-2xl">
@@ -57,6 +58,65 @@ const Contact = () => {
                         </ul>
                     </div>
                 ))}
+            </div>
+            <div className="flex flex-col md:flex-row items-center justify-center w-full my-12 gap-10 ">
+                <div className="grid gap-5 w-full rounded border border-neutral-400 p-5">
+                    <Title title1='Get In Touch' title2='With Us'/>
+                    <form className="grid gap-5 ">
+                        <input
+                        type="text"
+                        name='name'
+                        placeholder='Enter Your Name'
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  " />
+                        <input
+                        type="text"
+                        name='phone'
+                        placeholder='Enter Your Phone'
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  " />
+                        <input
+                        type="email"
+                        name='email'
+                        placeholder='Enter Your Email'
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  " />
+                        <textarea name="message" id="message" placeholder='Write the message here! ' rows={5}
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  "
+                        />
+                        <Button path='' label='Submit Now'  />
+                    </form>
+                </div>
+                <div className="grid gap-5 w-full rounded border border-neutral-400 p-5">
+                    <Title title1='Apply For' title2='a Position'/>
+                    <form className="grid gap-5 ">
+                        <input
+                        type="text"
+                        name='name'
+                        placeholder='Enter Your Name'
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  " />
+                        <input
+                        type="text"
+                        name='phone'
+                        placeholder='Enter Your Phone'
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  " />
+                        <input
+                        type="email"
+                        name='email'
+                        placeholder='Enter Your Email'
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  " />
+                        <input
+                        type="text"
+                        name='application'
+                        placeholder='Application For'
+                        className="text-neutral-500 border py-3 px-5 text-lg rounded  outline-none focus-within:border-secondary  " />
+                        <div className="grid gap-3">
+                            <h1 className="text-xl font-semibold ">Upload Your CV</h1>
+                            <div className="text-neutral-500 border py-3 px-5 text-lg rounded   focus-within:border-secondary">
+                                <input type="file" name="cv" id="cv" className='' />
+                            </div>
+
+                        </div>
+                        <Button path='' label='Submit Now'  />
+                    </form>
+                </div>
             </div>
         </div>
     </div>
