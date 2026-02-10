34e937a75681b614a1daf1e5855101b218d143ae
Amir Hamza <amirhamza27940@gmail.com>
2026-02-10 08:01:15 +0600

    referral page frontend

--- client/src/app/referral/page.tsx
+++ client/src/app/referral/page.tsx
@@ -1,15 +1,10 @@
 import PageBanner from 'app/UI/PageBanner'
 import Title from 'app/UI/Title'
+import { nav_items } from 'config/page'
 import React from 'react'
 import { FaCheck } from 'react-icons/fa'
 
 const Referral = () => {
-    const referral_information = [
-        {
-            title:"",
-
-        }
-    ]
   return (
     <div className="flex flex-col items-center justify-between w-full">
         <PageBanner title='Referral' path='referral'/>
@@ -117,10 +112,12 @@ const Referral = () => {
                     <div className=" grid gap-2 text-xl text-secondary-text w-full  ">
                     <label className="font-medium">Type Of Primary Service Required:</label>
                     <select name="gender" id="gender" className="px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="prefer not to say">Prefer not to say </option>
-                        </select>
+                            <option value="">Please Select </option>
+                            {nav_items[2].options?.map((item:any,index:number)=>(
+                                <option key={index} value={item.label} >{item.label}</option>
+                            ))}
+                            <option value="other">Other</option>
+                    </select>
                     </div>
                     <div className="grid gap-2 text-xl text-secondary-text w-full  ">
                         <label className="font-medium">Number Of Hours Requested For Service:</label>
@@ -130,18 +127,22 @@ const Referral = () => {
                     <div className="grid gap-2 text-xl text-secondary-text w-full  ">
                         <label className="font-medium">Type Of Secondary Service Required:</label>
                         <select name="gender" id="gender" className="px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="prefer not to say">Prefer not to say </option>
-                        </select>
+                            <option value="">Please Select </option>
+                            {nav_items[2].options?.map((item:any,index:number)=>(
+                                <option key={index} value={item.label} >{item.label}</option>
+                            ))}
+                            <option value="other">Other</option>
+                    </select>
                     </div>
                     <div className="grid gap-2 text-xl text-secondary-text w-full  ">
                         <label className="font-medium">Additional Service Required:</label>
                         <select name="gender" id="gender" className="px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="prefer not to say">Prefer not to say </option>
-                        </select>
+                            <option value="">Please Select </option>
+                            {nav_items[2].options?.map((item:any,index:number)=>(
+                                <option key={index} value={item.label} >{item.label}</option>
+                            ))}
+                            <option value="other">Other</option>
+                    </select>
                     </div>
                     <div className="grid gap-2 text-xl text-secondary-text w-full  ">
                         <label className="font-medium">Participant's Relevant Conditions / Disability (Please List):</label>
@@ -165,11 +166,12 @@ const Referral = () => {
                     </div>
                 </div>
             </div>
-            {/* participant details ends */}
+            {/* service request ends */}
 
-            <div className="grid  border border-neutral-300 px-10 py-5 rounded-md  my-10 w-full max-w-3xl">
+            <div className="flex flex-col gap-y-8  border border-neutral-300 px-10 py-5 rounded-md  my-10 w-full max-w-3xl">
                 <Title title1='Contact' title2='Details' />
-                <div className="">
+                <div className="flex flex-col md:flex-row justify-between w-full h-full items-start">
+                <div className="grid gap-4">
                     <label className="flex gap-3 text-xl font-medium text-secondary-text items-center cursor-pointer ">
                         <input type="checkbox" name="phone" id="phone" className=' peer hidden ' />
                         <div className=" border-3 h-5 text-[15px] flex rounded-full  text-secondary-text peer-checked:border-primary  peer-checked:text-white peer-checked:bg-primary m-0 p-0.5 w-5 items-center justify-center">
@@ -194,33 +196,59 @@ const Referral = () => {
                         <h4 className="">Text</h4>
                     </label>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 min-w-full gap-5 place-content-center">
-                    <div className=" grid gap-2 text-xl text-secondary-text w-full  ">
-                    <label className="font-medium">Participant Main Language Spoken</label>
-                    <input type="text" name="first_name" id="first_name"
-                    className=' px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ' />
-                    </div>
+                <div className="grid grid-cols-1 max-w-full gap-5 place-content-center">
                     <div className="grid gap-2 text-xl text-secondary-text w-full  ">
-                        <label className="font-medium">Does The Participant Require An Interpreter?</label>
-                        <select name="gender" id="gender" className="px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="prefer not to say">Prefer not to say </option>
+                        <label className="font-medium">Who Should We Contact To follow up on request?</label>
+                        <select name="contactor" id="contactor" className="px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ">
+                            <option value="participant_nominee">Participant/Nominee</option>
+                            <option value="support_coordinator">Support Coordinator</option>
+                            <option value="other">Other </option>
                         </select>
                     </div>
                     <div className="grid gap-2 text-xl text-secondary-text w-full  ">
-                        <label className="font-medium">Does The Participant Require An Interpreter?</label>
-                        <input type='phone' name="participant_phone" id="participant_phone"
+                        <input type='text' name="follow_up_name" id="follow_up_name" placeholder='Name'
                         className=' px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ' />
                     </div>
                     <div className="grid gap-2 text-xl text-secondary-text w-full  ">
-                        <label className="font-medium">Does The Listed Participant Identify As An Aboriginal Or Torres Strait Islander?</label>
+                        <input type='phone' name="follow_up_phone" id="follow_up_phone" placeholder='Phone'
+                        className=' px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ' />
+                    </div>
+                    <div className="grid gap-2 text-xl text-secondary-text w-full  ">
+                        <input type='email' name="follow_up_email" id="follow_up_email" placeholder='Email'
+                        className=' px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ' />
+                    </div>
+                </div>
+
+                </div>
+                <div className="grid gap-2 text-xl text-secondary-text w-full col-span-2  ">
+                        <label className="font-medium">Notes For Reception Staff (If Applicable):</label>
+                        <textarea rows={5} name="notes" id="notes"
+                        className=' px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ' />
+                    </div>
+            </div>
+            {/* participant details */}
+
+            <div className="grid  border border-neutral-300 px-10 py-5 rounded-md  my-10 w-full max-w-3xl">
+                <Title title1='NDIS' title2=' Information' />
+                <div className="grid grid-cols-1  min-w-full gap-5 place-content-center">
+                    <div className="grid gap-2 text-xl text-secondary-text w-full  ">
+                        <label className="font-medium">Does The Participant Require An Interpreter?</label>
                         <select name="gender" id="gender" className="px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded  min-w-full ">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="prefer not to say">Prefer not to say </option>
+                            <option value="">Please Select</option>
+                            <option value="male">Yes</option>
+                            <option value="female">No</option>
                         </select>
                     </div>
+                    <div className="grid gap-2 text-xl text-secondary-text w-full cursor-pointer  ">
+                        <h2 className="">Does The Participant Require An Interpreter?</h2>
+                        <label htmlFor='ndis_document' className="font-medium">
+                        </label>
+                        <input type='file' name="ndis_document" id="ndis_document"
+                        className=' px-4 py-3 border-neutral-400 focus-within:outline-secondary border rounded cursor-pointer   min-w-full ' />
+                    </div>
+
+
+
                 </div>
             </div>
             {/* participant details */}
